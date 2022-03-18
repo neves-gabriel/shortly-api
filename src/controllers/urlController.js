ba8891b6591ec8e667c1456e062e59d918a196e9
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-03-18 01:09:33 -0300

    fix: :bug: fixed query

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -31,14 +31,16 @@ export async function getShortUrl(req, res) {
   const { shortUrl } = req.params;
 
   try {
-    const result = await connection.query(
-      `SELECT id, shorturl, url FROM "urls" WHERE shorturl = $1;`[shortUrl]
+    const shortenedUrl = await connection.query(
+      `SELECT id, "shortUrl", url FROM "urls" WHERE "shortUrl" = $1;`,
+      [shortUrl]
     );
 
-    if (result.rowCount === 0) {
+    if (!shortenedUrl.rowCount) {
       return res.sendStatus(404);
     }
-    res.status(200).send(result.rows[0]);
+
+    res.status(200).send(shortenedUrl.rows[0]);
   } catch (error) {
     res.sendStatus(500);
   }
