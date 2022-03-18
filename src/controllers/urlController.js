289e4d19b8ef0bcadbee2103ffc0911a454a2e38
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-03-18 01:30:53 -0300

    feat: :sparkles: added delete shortUrl

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,10 +2,10 @@ import { nanoid } from "nanoid";
 import { connection } from "../database.js";
 
 export async function postUrl(req, res) {
-  try {
-    const { user } = res.locals;
-    const { url } = req.body;
+  const { user } = res.locals;
+  const { url } = req.body;
 
+  try {
     const shortUrl = nanoid(8);
 
     await connection.query(
@@ -43,5 +43,26 @@ export async function getShortUrl(req, res) {
     res.status(200).send(shortenedUrl.rows[0]);
   } catch (error) {
     res.sendStatus(500);
+  }
+}
+
+export async function deleteShortUrl(req, res) {
+  const { id } = req.params;
+  const { user } = res.locals;
+
+  try {
+    const result = await connection.query(
+      `DELETE FROM "urls" WHERE "urls".id = $1 AND "userId" = $2 RETURNING *;`,
+      [id, user.id]
+    );
+
+    if (!result.rowCount) {
+      return res.sendStatus(401);
+    }
+
+    return res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
   }
 }
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { postUrl, getShortUrl } from "../controllers/urlController.js";
+import {
+  postUrl,
+  getShortUrl,
+  deleteShortUrl,
+} from "../controllers/urlController.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
 const urlRouter = Router();
 urlRouter.post("/urls/shorten", validateTokenMiddleware, postUrl);
 urlRouter.get("/urls/:shortUrl", getShortUrl);
+urlRouter.delete("/urls/:id", validateTokenMiddleware, deleteShortUrl);
 export default urlRouter;
